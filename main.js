8c59cf5db219ddd3ed8f8d6e449e9589ba528282
Ahmed-hessen <128532764+Ahmed-hessen@users.noreply.github.com>
2023-06-15 00:16:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+let input  = document.getElementById("input") ;
+
+let exp = "";
+
+function press (num) {
+    exp += num;
+    input.value = exp;
+}
+
+function equal (){
+    let result = eval(exp);
+    input.value = result;
+    exp="";
+}
+function erase() {
+    exp = "";
+    input.value = exp;
+}
